e5ff5cad1dd5828b5a58437f2771ce60fdbaaecc
= <=>
2025-03-14 16:48:39 +0700

    Add API Station Schedules

--- module/station/router.go
+++ module/station/router.go
@@ -13,9 +13,33 @@ func Initiate(router *gin.RouterGroup) {
 	station.GET("", func(c *gin.Context) {
 		GetAllStation(c, stationService)
 	})
+
+	station.GET("/:id", func(c *gin.Context) {
+		CheckSchedulesByStation(c, stationService)
+	})
 }
 func GetAllStation(c *gin.Context, service Service) {
 	datas, err := service.GetAllStation()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    struct{}{},
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, response.APIResponse{
+		Success: true,
+		Message: "success",
+		Data:    datas,
+	})
+}
+
+func CheckSchedulesByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckScheduleByStation(id)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, response.APIResponse{
 			Success: false,
--- module/station/service.go
+++ module/station/service.go
@@ -2,13 +2,16 @@ package station
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/mfajrihusaini02/mrt-schedules/common/client"
 	"net/http"
+	"strings"
 	"time"
 )
 
 type Service interface {
-	GetAllStation() (response []Response, err error)
+	GetAllStation() (response []StationResponse, err error)
+	CheckScheduleByStation(id string) (response []ScheduleResponse, err error)
 }
 
 type service struct {
@@ -23,7 +26,7 @@ func NewService() Service {
 	}
 }
 
-func (s *service) GetAllStation() (response []Response, err error) {
+func (s *service) GetAllStation() (response []StationResponse, err error) {
 	url := "https://www.jakartamrt.co.id/id/val/stasiuns"
 
 	byteResponse, err := client.DoRequest(s.client, url)
@@ -35,10 +38,110 @@ func (s *service) GetAllStation() (response []Response, err error) {
 	err = json.Unmarshal(byteResponse, &stations)
 
 	for _, item := range stations {
-		response = append(response, Response{
+		response = append(response, StationResponse{
 			Id:   item.Id,
 			Name: item.Name,
 		})
+	}
+
+	return
+}
+
+func (s *service) CheckScheduleByStation(id string) (response []ScheduleResponse, err error) {
+	url := "https://www.jakartamrt.co.id/id/val/stasiuns"
+
+	byteResponse, err := client.DoRequest(s.client, url)
+	if err != nil {
+		return
+	}
+
+	var schedule []Schedule
+	err = json.Unmarshal(byteResponse, &schedule)
+	if err != nil {
+		return
+	}
+
+	var scheduleSelected Schedule
+	for _, item := range schedule {
+		if item.StationId == id {
+			scheduleSelected = item
+			break
+		}
+	}
+
+	if scheduleSelected.StationId == "" {
+		err = errors.New("station not found")
+	}
+
+	response, err = ConvertDataToResponses(scheduleSelected)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func ConvertDataToResponses(schedule Schedule) (response []ScheduleResponse, err error) {
+	var (
+		LebakBulusTripName = "Station Lebak Bulus Grab"
+		BundarahHITripName = "Station Bundarah HI Bank DKI"
+	)
+
+	scheduleLebakBulus := schedule.ScheduleLebakBulus
+	scheduleBundaranHI := schedule.ScheduleBundaranHI
+
+	scheduleLebakBulusParsed, err := ConvertScheduleToTimeFormat(scheduleLebakBulus)
+	if err != nil {
+		return
+	}
+
+	scheduleBundaranHIParsed, err := ConvertScheduleToTimeFormat(scheduleBundaranHI)
+	if err != nil {
+		return
+	}
+
+	for _, item := range scheduleLebakBulusParsed {
+		if item.Format("15:04") > time.Now().Format("15:04") {
+			response = append(response, ScheduleResponse{
+				StationName: LebakBulusTripName,
+				Time:        item.Format("15:04"),
+			})
+		}
+
+	}
+
+	for _, item := range scheduleBundaranHIParsed {
+		if item.Format("15:04") > time.Now().Format("15:04") {
+			response = append(response, ScheduleResponse{
+				StationName: BundarahHITripName,
+				Time:        item.Format("15:04"),
+			})
+		}
+
+	}
+
+	return
+}
+
+func ConvertScheduleToTimeFormat(schedule string) (response []time.Time, err error) {
+	var (
+		parsedTime time.Time
+		schedules  = strings.Split(schedule, ",")
+	)
+
+	for _, item := range schedules {
+		trimedTime := strings.TrimSpace(item)
+		if trimedTime == "" {
+			continue
+		}
+
+		parsedTime, err = time.Parse("15:04", trimedTime)
+		if err != nil {
+			err = errors.New("invalid time format " + trimedTime)
+			return
+		}
+
+		response = append(response, parsedTime)
 	}
 
 	return
